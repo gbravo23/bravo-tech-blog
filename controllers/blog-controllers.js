2d7252f6ac481a14598fa1143aa4ad49b5f8f8ad
Bravo <gmb2388@yahoo.com>
2021-03-21 15:31:51 -0700

    finished updating partials and layouts

---
+++ controllers/blog-controllers.js
@@ -0,0 +1,25 @@
+// Prob don't need
+
+const express = require("express");
+
+const router = express.Router();
+
+// render views based on the routes hit
+
+
+
+router.get("/", function (req, res) {
+    // set the data you want to display into a data const and pass it along with the page name in res.render
+    res.render("home",)
+})
+
+
+
+// /login
+
+// /signup
+
+// /logout
+
+// /dashboard
+
