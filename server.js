f568d7c548280a51d2a1e515350b5798e8dae0ca
Bravo <gmb2388@yahoo.com>
2021-03-21 15:40:20 -0700

    finished updating views

--- server.js
+++ server.js
@@ -1,33 +1,65 @@
-const express = require("express");
-const PORT = process.env.PORT || 3000;
+const path = require('path'); // added for front end
+const express = require('express');
+const routes = require('./controllers/');
+// inporting the connection to sequilize
+const sequelize = require('./config/connection');
 
-var app = express();
-// middleware
-app.use(express.static("public"));
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(express.json());
+const session = require('express-session');
 
-app.use(
-    session({
-        secret: 'This is a major secret!',
-        resave: false,
-        saveUninitialized: false
+// added for front end
+const exphbs = require('express-handlebars');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// maxAge allows for timeout of session if idle for 30 seconds
+const sess = {
+    //   secret: 'Super secret secret',
+    //   cookie: {},
+    //   resave: false,
+    //   saveUninitialized: true,
+    //   store: new SequelizeStore({
+    //     db: sequelize
+    //   })
+    // };
+
+    secret: 'Super secret secret',
+    cookie: {
+        maxAge: 60000
+    },
+    resave: false,
+    saveUninitialized: true,
+    rolling: true,
+
+    store: new SequelizeStore({
+        db: sequelize
     })
-);
+};
 
-var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({
-    defaultLayout: "main"
-}));
+app.use(session(sess));
 
-app.set("view engine", "handlebars");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const routes = require("./controllers/blog-controllers");
+// bring in front end
+app.use(express.static(path.join(__dirname, 'public')));
 
+// turn on routes
 app.use(routes);
 
-app.listen(PORT, function () {
-    console.log("Server listening on PORT" + PORT)
+
+// turn on connection to db and server
+// force:true, drops and re-creates all of the database tables and associations on startup.
+// same as DROP TABLE IF EXISTS in sql
+// necessary if have new table associations
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
 });
