58637f23f8c7081e83a1d4a3bb44e1ef291bc54c
Bravo <gmb2388@yahoo.com>
2021-03-21 21:07:07 -0700

    troublshooting heroku

--- models/user.js
+++ models/user.js
@@ -2,8 +2,6 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-// create our User model
-
 class User extends Model {
     checkPassword(loginPw) {
         return bcrypt.compareSync(loginPw, this.password);
